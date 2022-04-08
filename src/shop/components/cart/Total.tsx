5a9ee911486f5b0528421f7d959a5ccf36fa4151
Agustín Vega <agustinvegadev@gmail.com>
2022-04-08 17:08:28 -0300

    Cart total now is functional

--- src/shop/components/cart/Total.tsx
+++ src/shop/components/cart/Total.tsx
@@ -1,11 +1,21 @@
+import { useSelector } from 'react-redux';
+import { IRootState } from '../../../store/interface/IRootState';
+
 export const Total = () => {
+
+    const cart = useSelector((state:IRootState) => state.cart);
+
+    const subtotal = cart.reduce((acc, product) => acc + (product.price * product.quantity), 0);
+    const SHIPPING = 50;
+
+
     return (
         <div className="max-w-[520px] mt-12 mx-auto flex flex-col gap-4">
             <div className="flex justify-between  border-b pb-2">
                 <h2 className="text-primary text-[20px] font-text">Subtotal</h2>
 
                 <div className="flex gap-2 items-center">
-                    <span className="text-primary texttext-primary text-[22px] font-bold">$500</span>
+                    <span className="text-primary texttext-primary text-[22px] font-bold">${subtotal}</span>
                     <span className="text-primary texttext-primary text-[15px]">UY</span>
                 </div>
             </div>
@@ -23,7 +33,7 @@ export const Total = () => {
                 <h2 className="text-primary text-[20px] font-title font-bold">Total</h2>
 
                 <div className="flex gap-2 items-center">
-                    <span className="text-primary texttext-primary text-[22px] font-bold">$550</span>
+                    <span className="text-primary texttext-primary text-[22px] font-bold">${subtotal + SHIPPING}</span>
                     <span className="text-primary texttext-primary text-[15px]">UY</span>
                 </div>
             </div>
